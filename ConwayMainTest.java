4376a39b583e92e1ecfd53751cb59e387e533720
shyam-77 <72121828+shyam-77@users.noreply.github.com>
2020-12-29 18:45:09 +0530

    Add files via upload

---
+++ ConwayMainTest.java
@@ -0,0 +1,19 @@
+package conwayJavaFX;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ConwayMainTest {
+
+	@Test
+	void testStart() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
